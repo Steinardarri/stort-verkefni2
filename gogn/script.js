07076d5b6d091cbec2f2b019042c46c9732b25bb
Steinardarri <Steinardth@gmail.com>
2017-11-15 15:49:04 +0000

    Fyrsti Dæmatími

--- gogn/script.js
+++ gogn/script.js
@@ -6,4 +6,32 @@ class Videos {
 
 class Player {
 
+
+
+
+    fullscreen() {
+      const { video } = this;
+
+      if (video.requestFullscreen) {
+        video.requestFullscreen();
+      } else if (video.mozRequestFullscreen) {
+        video.mozRequestFullscreen();
+      } else if (video.webkitRequestFullscreen) {
+        video.webkitRequestFullscreen();
+      }
+    }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const videos = new Videos();
+  const player = new Player();
+
+  const videosContainer = document.querySelector('.videos');
+
+  if (videosContainer) {
+    videos.load();
+  } else {
+    player.load();
+    player.controls();
+  }
+});
