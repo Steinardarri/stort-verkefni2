cfc25d505583b9a2f3f14c4dabb40c2da10ef0a3
Biggatron <32528225+Biggatron@users.noreply.github.com>
2017-11-15 15:48:45 +0000

    Dæmatími

--- gogn/script.js
+++ gogn/script.js
@@ -7,3 +7,24 @@ class Videos {
 class Player {
 
 }
+
+load() {
+  const request = new XMLHttpRequest();
+
+  request.open('GET', url, true);
+  request.onload = () => {
+    if (request.status >= 200 && request.status <400) {
+      const data = JSON.parse(request.response);
+      this.loadCategories(data.videos, data.categories)
+    }
+    else
+    this.error
+    }
+  }
+
+  request.onerror = () => {
+    this.error();
+  }
+
+  request.send();
+}
