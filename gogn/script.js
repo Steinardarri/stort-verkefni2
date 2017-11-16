acae1da3e5bfb22c588865cb0f0821183a1602fd
Steinardarri <Steinardth@gmail.com>
2017-11-16 17:14:34 +0000

    Smá hreinsun og uppsetning

--- gogn/script.js
+++ gogn/script.js
@@ -2,45 +2,58 @@ const URL = './videos.json';
 
 class Videos {
 
+  loadCategories(videos, categories) {
+
+  }
+
+
+
+  load() {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', url, true);
+    request.onload = () => {
+      if (request.status >= 200 && request.status < 400) {
+        const data = JSON.parse(request.response);
+        this.loadCategories(data.videos, data.categories)
+      } else
+        this.error();
+    }
+
+    request.onerror = () => {
+      this.error();
+    }
+
+    request.send();
+  }
+
 }
 
 class Player {
 
+  fullscreen() {
+    const {
+      video
+    } = this;
 
-
-
-    fullscreen() {
-      const { video } = this;
-
-      if (video.requestFullscreen) {
-        video.requestFullscreen();
-      } else if (video.mozRequestFullscreen) {
-        video.mozRequestFullscreen();
-      } else if (video.webkitRequestFullscreen) {
-        video.webkitRequestFullscreen();
-      }
+    if (video.requestFullscreen) {
+      video.requestFullscreen();
+    } else if (video.mozRequestFullscreen) {
+      video.mozRequestFullscreen();
+    } else if (video.webkitRequestFullscreen) {
+      video.webkitRequestFullscreen();
     }
-}
 
-load() {
-  const request = new XMLHttpRequest();
-
-  request.open('GET', url, true);
-  request.onload = () => {
-    if (request.status >= 200 && request.status <400) {
-      const data = JSON.parse(request.response);
-      this.loadCategories(data.videos, data.categories)
-    }
-    else
-    this.error
-    }
   }
 
-  request.onerror = () => {
-    this.error();
+  controls() {
+
   }
 
-  request.send();
+  load() {
+
+  }
+
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -55,4 +68,5 @@ document.addEventListener('DOMContentLoaded', () => {
     player.load();
     player.controls();
   }
+
 });
